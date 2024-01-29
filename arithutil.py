4cd117ad61e775e6dcacc47399915dda83b27878
Sirawit <plurm2545@hotmail.com>
2024-01-29 12:54:20 +0100

    feat: ring Z mod nZ

--- arithutil.py
+++ arithutil.py
@@ -1,4 +1,15 @@
 import random
+import math
+
+
+def euclid(b, n):
+    # Adapted from https://github.com/lapets/egcd/blob/main/src/egcd/egcd.py
+    (x0, x1, y0, y1) = (1, 0, 0, 1)
+    while n != 0:
+        (q, b, n) = (b // n, n, b % n)
+        (x0, x1) = (x1, x0 - q * x1)
+        (y0, y1) = (y1, y0 - q * y1)
+    return (x0, y0)
 
 
 class BaseFieldElem:
@@ -102,6 +113,54 @@ def init_prime_field(p):
         def __str__(self):
             return f'PrimeField({self.card})'
     return PrimeField()
+
+
+def init_ring_Z_mod_nZ(n):
+    # This is a fake field, but I don't want to implement another object called "rings"...
+    class Z_mod_nZ(BaseField):
+        char = n
+        card = n
+
+        class FieldElem(BaseFieldElem):
+            def __init__(self, x):
+                if isinstance(x, int):
+                    self.a = x % n
+                else:
+                    raise TypeError(f'Unknown type {type(x)}')
+
+            @classmethod
+            def add(cls, x, y):
+                return cls((x.a + y.a) % n)
+
+            @classmethod
+            def addinv(cls, x):
+                return cls((n - x.a) % n)
+
+            @classmethod
+            def mul(cls, x, y):
+                return cls((x.a * y.a) % n)
+
+            @classmethod
+            def mulinv(cls, x):
+                # If xy = kn + 1 for some k, then y corresponds to a in
+                # each solution (a, b) to the equation ax + bn = 1.
+                g = math.gcd(x, n)
+                # If gcd(x, n) != 1, raise a zero div error
+                if g != 1:
+                    raise ZeroDivisionError(f'{x} is not invertible in Z/{n}Z')
+                a, b = euclid(x, n)
+                return a
+
+            @classmethod
+            def eql(cls, x, y):
+                return x.a == y.a
+
+        def __init__(self):
+            self.FieldElem.field = self
+
+        def __str__(self):
+            return f'Z_mod_{self.card}Z'
+    return Z_mod_nZ()
 
 
 class Polynomial:
